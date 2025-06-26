502296a9d64706f5aaf7816d773a2932829a1ec4
Jerome Verhoeven <jerome.verhoeven@gmail.com>
2025-06-26 13:35:26 -0500

    added document exporter and deploy folder, and some examples

---
+++ Deploy/doc-export.bat
@@ -0,0 +1,6 @@
+ pandoc SES_909.md -o "SES 909.pdf" --pdf-engine=pdflatex -V geometry:landscape -V geometry:margin=0.5in
+ pandoc SES_909.md -o SES_909.html
+ pandoc SES_909.md -o "SES_909.docx" 
+ pandoc ../Develop/4044876.md -o "../Develop/4044876.pdf" --pdf-engine=pdflatex -V geometry:landscape -V geometry:margin=0.5in
+ pandoc ../4044701_README.md -o "../4044701_README.pdf" --pdf-engine=pdflatex -V geometry:landscape -V geometry:margin=0.5in
+ PAUSE
